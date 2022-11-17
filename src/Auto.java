f6752e1ebd5f8a8efdffa33f4aaf26b25ba66832
MrGibbs90 <mrgibbs9090@mail.ru>
2022-11-17 17:27:53 +0700

    ДЗ 3. Задание 2

--- src/Auto.java
+++ src/Auto.java
@@ -7,11 +7,29 @@ public class Auto {
     String country;
 
     public Auto(String brand, String model, double engineVolume, String color, int year, String country) {
+        if (brand == null || brand == "") {
+            brand = "default";
+        }
         this.brand = brand;
+        if (model == null || model == "") {
+            model = "default";
+        }
         this.model = model;
+        if (engineVolume <= 0) {
+            engineVolume = 1.5;
+        }
         this.engineVolume = engineVolume;
+        if (color == null || color == "") {
+            color = "белый";
+        }
         this.color = color;
+        if (year <= 0) {
+            year = 2000;
+        }
         this.year = year;
+        if (country == null || country == "") {
+            country = "default";
+        }
         this.country = country;
     }
 
